99780506add05efea7eb5c839ec197879673de75
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-19 07:27:19 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUpTW/SignIn.jsx
+++ client/src/components/LoginSignUpTW/SignIn.jsx
@@ -3,6 +3,7 @@ import { BsMortarboardFill } from "react-icons/bs";
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios'
 import  secureLocalStorage  from  "react-secure-storage";
+import './style.css'
 
 const SignIn = () => {
     const navigate = useNavigate()
@@ -42,45 +43,60 @@ const SignIn = () => {
             console.log(err)
         }
     }
-  return (
-    <div className='bg-gray-200 min-h-screen py-24 px-8'>
-        <div className="md:grid grid-cols-3 gap-2">
-            <div className=""></div>
-            <div className="">
-                <div className="bg-white py-16 px-8 rounded shadow-md w-full ">
-                    <center className='text-gray-500'>
-                        {/* change the Icon According to your needs */}
-                        <h1 className=''><BsMortarboardFill className='h-20 w-auto'/></h1>
-                        <p className="pt-4 text-2xl">Welcome Back</p>
-                        <p className="">Your Project Name</p>
-                    </center>
-                    <hr className='my-2'/>
-                    <div className="my-4">
-                        <form onSubmit={headleSubmit}>
-                            <div className="my-2 md:mx-8">
-                                <label htmlFor="" className=''>Email : </label>
-                                <input type="email" name="" id="" className="w-full h-12 pl-2 rounded bg-gray-200" required placeholder='Enter Email Address'
-                                onChange={e => SetLoginData({...LoginData, email:e.target.value})}/>
-                            </div>
-                            <div className="my-2 md:mx-8">
-                                <label htmlFor="" className=''>Password : </label>
-                                <input type="password" name="" id="" className="w-full h-12 pl-2 rounded bg-gray-200" required placeholder='Enter Password' 
-                                onChange={e => SetLoginData({...LoginData, password:e.target.value})}/>
-                            </div>
-                            <div className="my-2 md:mx-8">
-                                <button type='submit' className='mt-8 font-semibold w-full py-4 px-8 rounded bg-blue-500 text-white shadow-md duration-500 hover:bg-blue-600'>SignIn</button>
-                            </div>
-                        </form>
-                        <Link><p className="my-2 md:mx-8 text-blue-500 font-semibold">Forget Password ? </p></Link>
+
+    return (
+        <div className="bg-[#0b2a58] min-h-screen w-full">
+            <div className="containerLogin">
+                <div className="">
+                    <div className="imageDiv">
+                        asdasd
                     </div>
-                    <hr className='my-2'/>
-                    <p className="my-4">Don't have an Account ? <Link to={'/SignUp'}><span className="text-blue-500">SignUp</span></Link></p>
-                </div>  
+                    <div className="viewForm">
+                        asd
+                    </div>
+                </div>
             </div>
-            <div className=""></div>
         </div>
-    </div>
-  )
+    )
+//   return (
+//     <div className='bg-gray-200 min-h-screen py-24 px-8'>
+//         <div className="md:grid grid-cols-3 gap-2">
+//             <div className=""></div>
+//             <div className="">
+//                 <div className="bg-white py-16 px-8 rounded shadow-md w-full ">
+//                     <center className='text-gray-500'>
+//                         {/* change the Icon According to your needs */}
+//                         <h1 className=''><BsMortarboardFill className='h-20 w-auto'/></h1>
+//                         <p className="pt-4 text-2xl">Welcome Back</p>
+//                         <p className="">Your Project Name</p>
+//                     </center>
+//                     <hr className='my-2'/>
+//                     <div className="my-4">
+//                         <form onSubmit={headleSubmit}>
+//                             <div className="my-2 md:mx-8">
+//                                 <label htmlFor="" className=''>Email : </label>
+//                                 <input type="email" name="" id="" className="w-full h-12 pl-2 rounded bg-gray-200" required placeholder='Enter Email Address'
+//                                 onChange={e => SetLoginData({...LoginData, email:e.target.value})}/>
+//                             </div>
+//                             <div className="my-2 md:mx-8">
+//                                 <label htmlFor="" className=''>Password : </label>
+//                                 <input type="password" name="" id="" className="w-full h-12 pl-2 rounded bg-gray-200" required placeholder='Enter Password' 
+//                                 onChange={e => SetLoginData({...LoginData, password:e.target.value})}/>
+//                             </div>
+//                             <div className="my-2 md:mx-8">
+//                                 <button type='submit' className='mt-8 font-semibold w-full py-4 px-8 rounded bg-blue-500 text-white shadow-md duration-500 hover:bg-blue-600'>SignIn</button>
+//                             </div>
+//                         </form>
+//                         <Link><p className="my-2 md:mx-8 text-blue-500 font-semibold">Forget Password ? </p></Link>
+//                     </div>
+//                     <hr className='my-2'/>
+//                     <p className="my-4">Don't have an Account ? <Link to={'/SignUp'}><span className="text-blue-500">SignUp</span></Link></p>
+//                 </div>  
+//             </div>
+//             <div className=""></div>
+//         </div>
+//     </div>
+//   )
 }
 
 export default SignIn
