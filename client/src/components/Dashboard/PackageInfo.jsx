4845ceb2c3fa8fe6a550fb58a268d804ee66c073
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-20 08:00:32 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Dashboard/PackageInfo.jsx
+++ client/src/components/Dashboard/PackageInfo.jsx
@@ -10,19 +10,25 @@ const PackageInfo = () => {
                     <h1 className="font-semibold">Status : Active</h1>
                     <h1 className="font-semibold">Username : 94 812377305</h1>
                 </div>
-                <button className="bg-blue-700 w-full text-center rounded-xl shadow-md text-white font-semibold md:py-0 py-2 md:mt-0 mt-2">
-                    Package Upgrade
-                </button>
+                <a href="" className="w-full ">
+                    <button className="bg-blue-700 w-full text-center rounded-xl shadow-md text-white font-semibold md:py-6 py-2 md:mt-0 mt-2">
+                        Package Upgrade
+                    </button>
+                </a>
             </div>
         </div>
         <div className="p-4 bg-blue-600 my-2 rounded ">
             <div className="flex">
-                <button className="bg-blue-700 w-full text-center rounded-xl shadow-md text-white font-semibold h-full py-6 mr-2">
-                    Get Extra DB
-                </button>
-                <button className="bg-blue-700 w-full text-center rounded-xl shadow-md text-white font-semibold h-full py-6 ml-2">
-                    Get Data Add-ons
-                </button>
+                <a href="" className='w-full'>
+                    <button className="bg-blue-700 w-full text-center rounded-xl shadow-md text-white font-semibold h-full py-6 mr-2">
+                        Get Extra DB
+                    </button>
+                </a>
+                <a href="" className='w-full'>
+                    <button className="bg-blue-700 w-full text-center rounded-xl shadow-md text-white font-semibold h-full py-6 ml-2">
+                        Get Data Add-ons
+                    </button>
+                </a>
             </div>
         </div>
     </div>
--- client/src/components/Dashboard/SummaryData.jsx
+++ client/src/components/Dashboard/SummaryData.jsx
@@ -3,10 +3,36 @@ import DashSummary from './DashSummary'
 import PackageInfo from './PackageInfo'
 
 const SummaryData = () => {
+    const SummarySideData = [
+        {name: "My Package", BtnValue: 0, value: "96.4 used from 100.00 GB"},
+        {name: "Extra GB", BtnValue: 1, value: "0.9 used from 15.0 GB"},        
+        {name: "Bonus Data", BtnValue: 2, value: "1.3 used from 1.3 GB"},        
+        {name: "Add-Ons Data", BtnValue: 3, value: "6.3 used from 15.0 GB"},
+        {name: "Free Data", BtnValue: 4, value: "N/A"},
+        
+    ]
   return (
     <div>
         <DashSummary />
         <PackageInfo />
+
+        <div className="md:flex my-2">
+            <div className='w-1/3 mr-2'>
+                {
+                    SummarySideData.map((data, index) => {
+                        return (
+                            <div className="w-full bg-blue-600 p-4 rounded mr-1 mb-2 text-white cursor-pointer" key={index}>
+                                <p className="text-xl font-semibold text-center">{data.name}</p>
+                                <p className="text-center mt-2">{data.value}</p>
+                            </div>  
+                        )
+                    })
+                }
+            </div>
+            <div className="w-full bg-blue-600 p-4 rounded ml-1">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam cum aspernatur accusantium, hic enim numquam, sit repellat ab, nam consequuntur minus corrupti modi commodi quia qui aliquid earum repudiandae blanditiis?
+            </div>
+        </div>
     </div>
   )
 }
