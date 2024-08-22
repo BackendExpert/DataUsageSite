99f45ea1127b52b1a87607bb91f6384edc055fb3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-22 15:03:45 +0530

    Update DashHome.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Dashboard/Admin/DashHome.jsx
+++ client/src/components/Dashboard/Admin/DashHome.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
 
 const DashHome = () => {
+    const DashData = [
+      {
+        id: 1,
+        img: '',
+        text1: 'User Accouts',
+        text1Value: 900,
+        text2: 'Today',
+        text2Value: '12',
+        text3: 'Rate',
+        text3Value: '12.23 %'
+      },
+      {
+        id: 2,
+        img: '',
+        text1: 'User Accouts',
+        text1Value: 900,
+        text2: 'Today',
+        text2Value: '12',
+        text3: 'Rate',
+        text3Value: '12.23 %'
+      },
+      {
+        id: 3,
+        img: '',
+        text1: 'User Accouts',
+        text1Value: 900,
+        text2: 'Today',
+        text2Value: '12',
+        text3: 'Rate',
+        text3Value: '12.23 %'
+      },
+      {
+        id: 4,
+        img: '',
+        text1: 'User Accouts',
+        text1Value: 900,
+        text2: 'Today',
+        text2Value: '12',
+        text3: 'Rate',
+        text3Value: '12.23 %'
+      },
+    ]
   return (
-    <div>DashHome</div>
+    <div>
+        <h1 className="text-xl font-seimbold text-[#1F2B3C]">Dashboard</h1>
+    </div>
   )
 }
 
