78d6768fa5400d2eaba8635bbcdee428ac77abb9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-22 15:50:52 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Dashboard/Admin/DashHome.jsx
+++ client/src/components/Dashboard/Admin/DashHome.jsx
@@ -3,6 +3,7 @@ import UsersImg from '../../../assets/Users.png'
 import UsersRequestImg from '../../../assets/UserRequest.png'
 import Income from '../../../assets/Income.png'
 import DataUsage from '../../../assets/DataUsage.png'
+import { BsCaretUpFill } from 'react-icons/bs'
 
 const DashHome = () => {
     const DashData = [
@@ -31,7 +32,8 @@ const DashHome = () => {
       {
         id: 3,
         img: DataUsage,
-        text1: 'User Accouts',
+        text1: 'Data Usage',
+        text1Value: "GB",
         text2: 'Today',
         text2Value: '12',
         text3: 'Rate',
@@ -42,6 +44,7 @@ const DashHome = () => {
         id: 4,
         img: Income,
         text1: 'Income',
+        text1Value: "$",
         text2: 'Today',
         text2Value: '12',
         text3: 'Rate',
@@ -57,7 +60,7 @@ const DashHome = () => {
             {
                 DashData.map((data, index) => {
                     return (
-                        <div className={`py-4 px-6 rounded shadow-md ${data.bgColor}`} key={index}>
+                        <div className={`py-4 px-6 rounded shadow-md md:my-0 my-2 ${data.bgColor}`} key={index}>
                             <div className="flex">
                                 <div className="">
                                     <img src={data.img} alt="" />
@@ -81,6 +84,28 @@ const DashHome = () => {
                     )
                 })
             }
+        </div>
+
+        <div className="">
+            <div className="md:flex">
+                <div className="w-1/3 pb-2 my-4 mr-2">
+                    <div className="py-2 px-3 bg-white rounded shadow-md mb-2">
+                        <BsCaretUpFill className='text-[#56AAB3] h-16 w-full'/>
+
+                        <p className="text-center font-semibold text-gray-800">Last Month Income</p>
+
+                        <div className="text-center text-[#56AAB3] text-4xl py-4">
+                            55.23%
+                        </div>
+                    </div>
+                    <div className="py-2 px-3 bg-white rounded shadow-md mb-2">
+                        ahsjkdhajksdhkj 
+                    </div>
+                </div>
+                <div className="w-full bg-white rounded shadow-md py-2 px-4 my-4 ml-2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error impedit labore assumenda. Debitis enim, excepturi reprehenderit porro veniam, nulla dolore nihil earum dolor, asperiores adipisci corrupti minima consequuntur numquam laudantium.
+                </div>
+            </div>
         </div>
     </div>
   )
