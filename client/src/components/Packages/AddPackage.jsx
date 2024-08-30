438a4e610baadfae1e83fdc3ed4f38b04b3095c7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-30 16:08:49 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Packages/AddPackage.jsx
+++ client/src/components/Packages/AddPackage.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddPackage = () => {
+    const [PackageData, SetPackageData] = useState({
+        packageName: '',
+        packageDesc: '',
+        packageData: '',
+        freeData: '',
+        standedData: '',
+        packagePrice: '',
+        packageFor: ''
+    })
+
+    const headleCreatePackage = (e) => {
+        e.preventDefault();
+
+        
+    }
   return (
-    <div>AddPackage</div>
+    <div>
+        <h1 className="text-xl font-semibold text-gray-500">Create New Package</h1>
+
+        <div className="mt-4 bg-white rounded shadow-md p-4">
+
+        </div>
+    </div>
   )
 }
 
