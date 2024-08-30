77bb45d734b825badc58f16aa2e1eb80c90dbb44
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-30 16:02:09 +0530

    Update AccountData.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Accounts/AccountData.jsx
+++ client/src/components/Accounts/AccountData.jsx
@@ -8,7 +8,40 @@ const AccountData = () => {
         <h1 className="text-xl font-semibold text-gray-500">AccountData : {id} </h1>
 
         <div className="mt-4 bg-white rounded p-4 shadow-md">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit maiores, similique vero eum sed doloremque corporis blanditiis dicta ipsam eligendi quos non, debitis placeat. At officia omnis magni laborum ab!
+            <table className='w-full'>
+                <tr className='h-10 border-b border-gray-200'>
+                    <td className=''>
+                        <p className="text-gray-500 font-bold">Email Address : </p>
+                    </td>
+                    <td className=''>
+                        <p className="text-gray-500">{id}</p>
+                    </td>
+                </tr>
+                <tr className='h-10 border-b border-gray-200'>
+                    <td className=''>
+                        <p className="text-gray-500 font-bold">Account Number : </p>
+                    </td>
+                    <td className=''>
+                        <p className="text-gray-500">{id}</p>
+                    </td>
+                </tr>
+                <tr className='h-10 border-b border-gray-200'>
+                    <td className=''>
+                        <p className="text-gray-500 font-bold">Account Status : </p>
+                    </td>
+                    <td className=''>
+                        <p className="text-gray-500">Active</p>
+                    </td>
+                </tr>
+                <tr className='h-10 border-b border-gray-200'>
+                    <td className=''>
+                        <p className="text-gray-500 font-bold">Account Packages : </p>
+                    </td>
+                    <td className=''>
+                        <p className="text-gray-500">Fiber, PeoTV</p>
+                    </td>
+                </tr>
+            </table>
         </div>
     </div>
   )
