d46fbad408c17698641da1f9b6671ffea5055f0c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-08-30 15:56:33 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Accounts/AccountData.jsx
+++ client/src/components/Accounts/AccountData.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const AccountData = () => {
+    const {id} = useParams()
   return (
-    <div>AccountData</div>
+    <div>
+        <h1 className="text-xl font-semibold text-gray-500">AccountData : {id} </h1>
+
+        <div className="mt-4 bg-white rounded p-4 shadow-md">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit maiores, similique vero eum sed doloremque corporis blanditiis dicta ipsam eligendi quos non, debitis placeat. At officia omnis magni laborum ab!
+        </div>
+    </div>
   )
 }
 
