207580149e4a895cc9a1b11baa68e90f83c74578
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-02 11:10:33 +0530

    Update Package.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Models/Package.js
+++ server/Models/Package.js
@@ -1,7 +1,35 @@
 const mongoose = require('mongoose');
 
 const PackageSchema = new mongoose.Schema({
-
+    PackageName: {
+        type: String,
+        required: true,
+        unique:true,
+    },
+    pkData: {
+        type: Number,
+        required: true,
+    },
+    pkDesc: {
+        type: String,
+        required: true,
+    },
+    pkFreeData: {
+        type: Number,
+        required: true,
+    },
+    pkStadndedData: {
+        type: Number,
+        required: true,
+    },
+    pkPrice: {
+        type: Number,
+        required: true,
+    },
+    pkFor: {
+        type: String,
+        required: true,
+    },
 });
 
 const Package = mongoose.model('Package', PackageSchema);
