6f62e7fb07b1200c594a6f48001c8e23acf3d1fa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-02 11:16:41 +0530

    Update PackageController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controllers/PackageController.js
+++ server/Controllers/PackageController.js
@@ -1,5 +1,42 @@
+const Package = require("../Models/Package");
+
 const PackageController = {
-  
+  CreatePackage: async(req, res) => {
+    try{
+        const {
+            packageName,
+            packageDesc,
+            dataPackage,
+            freeData,
+            standedData,
+            packagePrice,
+            packageFor
+        } = req.body
+
+        const checkPck = await Package.findOne({ PackageName:packageName })
+
+        if(checkPck){
+            return res.json({ Error: "This Package is Already in System"})
+        }
+        else{
+            const NewPackage = new Package({
+                PackageName: packageName,
+                pkData: dataPackage,
+                pkDesc: packageDesc,
+                pkFreeData: freeData,
+                pkStadndedData: standedData,
+                pkPrice: packagePrice,
+                pkFor: packageFor
+            })
+
+            const resultPackage = await NewPackage.save()
+            res.json({ Status: "Success"})
+        }
+    }
+    catch(err) {
+        console.log(err)
+    }
+  }
 };
 
 module.exports = PackageController;
