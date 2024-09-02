c42dab8047bb3ecab86b160f537b88fc20d80f96
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-02 11:54:14 +0530

    Update PackageRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Routes/PackageRoute.js
+++ server/Routes/PackageRoute.js
@@ -3,7 +3,7 @@ const PackageController = require('../Controllers/PackageController');
 
 const router = express.Router();
 
-router.post('CreatePackage', PackageController.CreatePackage)
+router.post('/CreatePackage', PackageController.CreatePackage)
 router.post('/ViewPackage', PackageController.AllPackages)
 
 module.exports = router;
