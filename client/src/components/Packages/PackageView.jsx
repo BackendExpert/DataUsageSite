45008465ae069d47d2a35b79df80b69ae1b07f78
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-03 09:47:23 +0530

    Update PackageView.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Packages/PackageView.jsx
+++ client/src/components/Packages/PackageView.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
 const PackageView = () => {
+    const {id} = useParams()
+    const [packageOneData, SetPackageOneData] = useState([])
+
+    useEffect(() => {
+      const res = axios.get(import.meta.env.VITE_APP_API + `/package/ViewOnPackge/${id}`)
+      .then(res => SetPackageOneData(res.data.Result))
+      .catch(err => console.log(err))
+    }, [])
+    
   return (
     <div>PackageInfo</div>
   )
