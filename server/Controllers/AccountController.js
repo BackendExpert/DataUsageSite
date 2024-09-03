a154250a7150ecc01c9949dbe00366cfc6ae2e0b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-03 19:33:45 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ server/Controllers/AccountController.js
@@ -0,0 +1,12 @@
+const AccountController = {
+    CreateAccount: async (req, res) => {
+        try{
+            
+        }
+        catch(err) {
+            console.log(err)
+        }
+    }
+};
+
+module.exports = AccountController;
