a0638b48d76fecaef2271be928baaa84d86129a6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-03 10:41:05 +0530

    Update PackageView.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Packages/PackageView.jsx
+++ client/src/components/Packages/PackageView.jsx
@@ -13,7 +13,66 @@ const PackageView = () => {
     }, [])
     
   return (
-    <div>{packageOneData.pkDesc}</div>
+    <div>
+      <div className="bg-white rounded shadow-md p-4">
+        <p className="text-gray-500">
+          <span className='font-semibold'>Package Data</span> : {packageOneData.PackageName}
+        </p>
+
+        <table className='w-full mt-4'>
+          <tr className='h-10 border-b border-gray-200'>
+            <td className='w-1/4 font-semibold'>Package Name</td>
+            <td className='text-gray-500 border-l border-gray-200 pl-4'>{packageOneData.PackageName}</td>
+          </tr>
+          <tr className='h-10 border-b border-gray-200'>
+            <td className='w-1/4 font-semibold'>Package Description</td>
+            <td className='text-gray-500 border-l border-gray-200 pl-4'>{packageOneData.pkDesc}</td>
+          </tr>
+          <tr className='h-10 border-b border-gray-200'>
+            <td className='w-1/4 font-semibold'>Package Data</td>
+            {
+              (() => {
+                if(packageOneData.pkFor === "Internet Unlimited"){
+                  return (
+                    <td className='text-gray-500 border-l border-gray-200 pl-4'><span className='text-red-500 font-semibold'>{packageOneData.pkData} GB / Day</span>, After {packageOneData.pkData} GB Speed 1 mbps</td>
+                  )
+                }
+                else{
+                  return (
+                    <td className='text-gray-500 border-l border-gray-200 pl-4'>{packageOneData.pkData} GB / Month </td>
+                  )
+                }
+              })()
+            }
+          </tr>
+          <tr className='h-10 border-b border-gray-200'>
+            <td className='w-1/4 font-semibold'>Package Data Structure</td>
+            {
+              (() => {
+                if(packageOneData.pkFor === "Internet Unlimited"){
+                  return (
+                    <>
+                      <td className='w-1/4 text-gray-500 border-l border-gray-200 pl-4'><span>Free Data : </span> Unlimited GB</td>
+                      <td className='w-1/4 text-gray-500 border-l border-gray-200 pl-4'><span>Standed Data : </span> Unlimited GB</td>
+                    </>
+
+                  )
+                }
+                else{
+                  return (
+                    <>
+                      <td className='w-1/4 text-gray-500 border-l border-gray-200 pl-4'><span>Free Data : </span>{packageOneData.pkFreeData} GB</td>
+                      <td className='w-1/4 text-gray-500 border-l border-gray-200 pl-4'><span>Standed Data : </span>{packageOneData.pkStadndedData} GB</td>
+                    </>
+                  )
+                }
+              })()
+            }
+            
+          </tr>
+        </table>
+      </div>
+    </div>
   )
 }
 
