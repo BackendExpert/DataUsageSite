ad25f4a948be4ad879debf703fb52f8e5e3e79cc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-24 09:21:06 +0530

    Update Navbar.jsx

--- client/src/components/Public/Defaults/Navbar.jsx
+++ client/src/components/Public/Defaults/Navbar.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-full bg-white shadow-md py-6 md:px-16 px-8'>
+        <div className="md:flex">
+            <div className="">
+                <h1 className="uppercase font-semibold text-[#6C63FF]">Internet Service</h1>
+            </div>
+        </div>
+    </div>
   )
 }
 
